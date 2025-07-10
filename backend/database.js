6a7e36d791c7c1d85b60cf5a0e8afc5392041512
Partha Kadam <parthakadam2007@gmail.com>
2025-07-11 02:10:39 +0530

    conncted local database

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
 
   connectionString: process.env.DATABASE_URL, // for Heroku or other platforms
-  ssl: { rejectUnauthorized: false } // required for Supabase
+  // ssl: { rejectUnauthorized: false } // required for Supabase
 })
 const bcrypt = require('bcrypt');
 console.log(`Database connected on port ${process.env.DATABASE_URL}`)
