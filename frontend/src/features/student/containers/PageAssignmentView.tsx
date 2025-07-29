581099166dba8a37bb9f29cc28b15288e6773128
pruthviraj-cpu <gawandepruthviraj9@gmail.com>
2025-07-29 22:45:42 +0530

    add edd imopert

--- frontend/src/features/student/containers/PageAssignmentView.tsx
+++ frontend/src/features/student/containers/PageAssignmentView.tsx
@@ -1,6 +1,7 @@
 import PageList from '../../../shared/components/sidebar/PageList';
 import SubmissionUpload from '../components/student-Assignment-view/SubmissionUpload';
 import ContentAssignment from '../components/student-Assignment-view/ContentAssignment';
+import Header from '../../../shared/components/header/Header';
 const PageAssignmentView = () => {
     return (
         <div>
@@ -17,3 +18,4 @@ const PageAssignmentView = () => {
         </div>
     );
 };
+export default PageAssignmentView;
