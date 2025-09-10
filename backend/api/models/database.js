ce31f365f53476a061027072c80c9f6b2834d38a
Partha Kadam <parthakadam2007@gmail.com>
2025-09-10 22:16:31 +0530

    Update database.js

--- backend/api/models/database.js
+++ backend/api/models/database.js
@@ -1,16 +1,4 @@
-// require('dotenv').config();
-// const Pool = require('pg').Pool
-// const pool = new Pool({
 
-//   connectionString: process.env.DATABASE_URL, // for Heroku or other platforms
-//   ssl: { rejectUnauthorized: false } // required for Supabase
-// })
-// const bcrypt = require('bcrypt');
-// console.log(`Database connected on port ${process.env.DATABASE_URL}`)
-// module.exports= {
-//     pool,
-//     bcrypt
-// }
 require('dotenv').config();
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
@@ -19,9 +7,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction
-    ? { rejectUnauthorized: false }  // only enable SSL for remote DBs
-    : false,                          // local DB does NOT need SSL
+  ssl: { rejectUnauthorized: false } // required for Supabase    // local DB does NOT need SSL
 });
 
 pool.on('connect', () => {
