ebf11be1ddf31e9862deec11ac08e6ebbbef16af
Partha Kadam <parthakadam2007@gmail.com>
2025-09-19 23:40:33 +0530

    typeFix

--- frontend/src/features/ClassTeacher/components/attendance/AllAttendaceClass.tsx
+++ frontend/src/features/ClassTeacher/components/attendance/AllAttendaceClass.tsx
@@ -1,8 +1,15 @@
 import useFetch from "../../../../shared/hooks/UseFetch";
 import AnimatedLoader from "../../../../shared/components/loaders/DefaultLoader";
 
+type AttendanceRecord = {
+  date: string;
+  lecture_number: number;
+  first_marked_time: string;
+  percentage_present: number;
+};
+
 const AllAttendaceClass: React.FC<{ class_id: string }> = ({ class_id }) => {
-  const { data, status } = useFetch({
+  const { data, status } = useFetch<AttendanceRecord[]>({
     method: "GET",
     url: `${import.meta.env.VITE_BACKEND_URL}/teacher/attendance/class/${class_id}`,
   });
