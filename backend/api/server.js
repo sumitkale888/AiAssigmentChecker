14d037b053d7dabec0e164c55eeaeb21755aae75
Partha Kadam <parthakadam2007@gmail.com>
2025-10-05 18:06:18 +0530

    added 'https://aiclassroom.online/'

--- backend/api/server.js
+++ backend/api/server.js
@@ -40,7 +40,8 @@ const allowedOrigins = [
   'https://ai-assigment-checker-zrth.vercel.app',
   'https://devaiassigmentchecker-c8bfdgd8h6bmdsad.canadacentral-01.azurewebsites.net',
   'https://ai-assigment-checker-wy6o-fpkfkq84d-parthakadam2007s-projects.vercel.app',
-  'http://ec2-65-0-205-222.ap-south-1.compute.amazonaws.com'
+  'http://ec2-65-0-205-222.ap-south-1.compute.amazonaws.com',
+  'https://aiclassroom.online/'
 ];
 
 app.use(cors({
