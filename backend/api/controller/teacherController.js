419441e0db13c2d56b52c52e4a48c8092dccaf67
Partha Kadam <parthakadam2007@gmail.com>
2025-10-10 22:46:16 +0530

    removed nodemailer

--- backend/api/controller/teacherController.js
+++ backend/api/controller/teacherController.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 const { pool } = require('../models/database');
 
 ///////////////////////POST ROUTES////////////////////////////////////////////////
